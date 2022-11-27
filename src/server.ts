5a9b820a35ed3de769a4298656167d4a657e7a70
Diego Sousa <derickbenji2@gmail.com>
2022-11-26 22:22:26 -0300

    Criando rotas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 import cors from "@fastify/cors";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -15,9 +17,36 @@ async function bootstrap() {
     origin: true,
   });
 
-  fastify.get("/pools/count", async () => {
-    const count = await prisma.pool.count();
+  fastify.get("/bets/count", async () => {
+    const count = await prisma.bet.count();
     return { count };
+  });
+
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+    return { count };
+  });
+
+  fastify.post("/bets", async (request, reply) => {
+    const createbetBody = z.object({
+      title: z.string(),
+    });
+    const { title } = createbetBody.parse(request.body);
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.bet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333, host: "0.0.0.0" });
